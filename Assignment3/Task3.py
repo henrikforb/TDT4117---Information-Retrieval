e7eaaabb526bbc3827187837f90b0e5a68c31c37
henrikforb <35838133+henrikforb@users.noreply.github.com>
2019-10-15 17:02:25 +0200

    Update Task3.py

---
+++ Assignment3/Task3.py
@@ -0,0 +1,23 @@
+"""
+Retrieval Models.
+This module convert Bags-of-Words into TF-IDF weights and
+then LSI(Latent Semantic Indexing) weights.
+"""
+
+import gensim
+
+# Task 3.1
+tfidf_model = gensim.models.TfidfModel(corpus)
+
+# Task 3.2 
+tdif_corpus = tfidf_model[corpus]
+
+# Task 3.3
+matrix_sim = gensim.similarities.MatrixSimilarity(corpus)
+
+# Task 3.4
+lsi_model = gensim.models.LsiModel(tfidf_corpus, id2word=dictonary, num_topics=100)
+lsi_corpus = gensim.similarities.MatrixSimilarity(tdif_corpus)
+
+# Task 3.5
+print(lsi_model.show_topics())
